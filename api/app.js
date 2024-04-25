325551faefafebedf4caa771488a80f7aa75c033
Akira <junrongfu6@gmail.com>
2024-04-25 15:57:41 +0800

    added:handle cors

--- api/app.js
+++ api/app.js
@@ -1,19 +1,25 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoutes from "./routes/post.route.js";
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-// app.use("/api/test", (req, res) => {
-//   res.send("This is the middleware, it works!");
-// });
+// Enable CORS middleware
+//TODO: check how to make cors work with dynamic env
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
+
+// Other middleware
 app.use(express.json());
 app.use(cookieParser());
 
+//routes
 app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
 
+// Start the server
 app.listen(8800, () => {
   console.log("Server is running!");
 });
