421f336dc7be115c2a49924df48f0a4b9019f0bf
Akira <junrongfu6@gmail.com>
2024-04-27 23:31:33 +0800

    added: fetching username & avatar when fetch post detail

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,7 +5,8 @@ import bcrypt from "bcrypt";
 export const getAllUsers = async (req, res) => {
   try {
     const users = await prisma.user.findMany();
-    res.status(200).json(users);
+    //TODO: find out the difference between with or without "return", both working
+    return res.status(200).json(users);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to get users" });
