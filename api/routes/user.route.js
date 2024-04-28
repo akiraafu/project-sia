2b219aeee1700b63af8becff48b16a7b54d4896b
Akira <junrongfu6@gmail.com>
2024-04-28 21:12:03 +0800

    added: profilePosts route and method

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,15 +5,17 @@ import {
   getOneUser,
   savePost,
   updateUser,
+  profilePosts,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
-router.get("/:id", verifyToken, getOneUser);
+// router.get("/:id", verifyToken, getOneUser);
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
 router.post("/save", verifyToken, savePost);
+router.get("/profile-posts", verifyToken, profilePosts);
 
 export default router;
